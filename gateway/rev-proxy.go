3619f894c56f22cd9d5fb04619fd7dff0ca5064b
snowtoslow <snowtoslow@gmail.com>
2021-10-03 10:53:34 +0300

    add jwt dependency to decode jtw token and extract permissions; rework a bit the gateway;

--- gateway/rev-proxy.go
+++ gateway/rev-proxy.go
@@ -4,6 +4,8 @@ import (
 	v1 "Yandex-Taxi-Clone/pkg/api/v1"
 	"bytes"
 	"context"
+	"fmt"
+	jwt "github.com/golang-jwt/jwt/v4"
 	"google.golang.org/grpc"
 	"io/ioutil"
 	"log"
@@ -17,14 +19,13 @@ import (
 func main() {
 	log.SetPrefix("[proxy] ")
 	log.SetOutput(os.Stdout)
-	url, _ := url.Parse("http://localhost:8086")
+	url, _ := url.Parse("http://localhost:8080")
 	path := "/*catchall"
 	director := func(req *http.Request) {
 		req.Header.Add("X-Forwarded-Host", req.Host)
 		req.Header.Add("X-Origin-Host", url.Host)
 		req.URL.Scheme = url.Scheme
 		req.URL.Host = url.Host
-		req.Header.Set("Content-Type", "application/grpc+protobuf")
 
 		wildcardIndex := strings.IndexAny(path, "*")
 		proxyPath := singleJoiningSlash(url.Path, req.URL.Path[wildcardIndex:])
@@ -32,24 +33,43 @@ func main() {
 			proxyPath = proxyPath[:len(proxyPath)-1]
 		}
 		req.URL.Path = proxyPath
-		req.Proto = "HTTP/2.0"
 	}
 	proxy := &Upstream{target: url, proxy: &httputil.ReverseProxy{
 		Director: director,
-		Transport: &CustomProtocol{
-			Host:    "localhost:8086",
-			Context: context.Background(),
-		},
 		ModifyResponse: func(response *http.Response) error {
 			log.Printf("%+v", response)
 			return nil
 		},
 	}}
 
-	/*mux := http.NewServeMux()
-	mux.HandleFunc("/", proxy.handle)
-	log.Fatal(http.ListenAndServe(":9001", mux))*/
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.URL.Path, "/api/auth") {
+			if tokenString := r.Header.Get("Authorization"); len(tokenString) != 0 {
+				token, err := jwt.Parse(tokenString[7:], func(token *jwt.Token) (interface{}, error) {
+					// Don't forget to validate the alg is what you expect:
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+					}
+
+					return []byte{97, 100, 56, 100, 53, 98, 55, 56, 49, 49, 98, 49, 53, 57, 54, 53, 97, 101, 55, 49, 102, 102, 52, 55, 98, 51, 99, 57, 101, 102, 56, 97, 56, 50, 97, 102, 53, 99, 56, 53, 102, 55, 99, 55, 49, 102, 100, 100, 55, 50, 52, 102, 102, 102, 97, 100, 57, 100, 99, 52, 97, 57, 53, 57, 56, 97, 52, 49, 100, 54, 101, 57, 55, 98, 53, 97, 49, 52, 48, 98, 56, 98, 48, 56, 55, 98, 55, 102, 48, 56, 99, 50, 98, 100, 55, 56, 101, 56, 99, 98, 54, 56, 57, 49, 57, 99, 56, 55, 48, 48, 48, 48, 54, 57, 57, 97, 102, 98, 100, 50, 54, 52, 49, 98, 100, 98, 102, 56}, nil
+				})
+
+				if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+					fmt.Println("Claims: ", claims)
+				} else {
+					fmt.Println(err)
+				}
+
+			} else {
+				log.Printf("EMPTY TOKEN")
+			}
+			r.Proto = "HTTP/2.0"
+			proxy.proxy.Transport = &CustomProtocol{
+				Host:    "localhost:8086",
+				Context: context.Background(),
+			}
+		}
+
 		proxy.proxy.ServeHTTP(w, r)
 	})
 
@@ -98,7 +118,6 @@ func (custom *CustomProtocol) RoundTrip(req *http.Request) (*http.Response, erro
 		log.Println("error:", err)
 		return nil, err
 	}
-	log.Println("AAAA: ", a)
 
 	//Create custom httpResponses for status ok, unautorized and bad request
 	return &http.Response{
